94ade6ba64d71e8537d79990112a73ce242b0a55
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-09-08 02:32:36 +0500

    Верстка шапки

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,22 @@
+const Header = class Header {
+    constructor({isMobileMenuOpened}){
+        this.isMobileMenuOpened = false;
+    }
+    toogleMobileMenu() {
+        this.isMobileMenuOpened = !this.isMobileMenuOpened;
+    }
+    closeMobileMenu() {
+        this.isMobileMenuOpened = false;
+    }
+    setEventListener() {
+        document.addEventListener('click', (event) => {
+            if (event.target.closest('.header__burger--body') || event.target.closest('.header__burger')) return;
+            this.closeMobileMenu();
+        }) 
+    }
+    init() {
+        this.setEventListener();
+    }
+}
+
+export default Header;
