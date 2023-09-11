5e09d187e1db66108a4129232a73239cc4078436
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-09-11 09:34:57 +0500

    Правки

--- src/blocks/modules/main_tabs/main_tabs.js
+++ src/blocks/modules/main_tabs/main_tabs.js
@@ -1,36 +1,38 @@
 const MainTabs = class MainTabs {
-    constructor(){}
-    openTabs() {
-        document.addEventListener('DOMContentLoaded', function () {
-            const tabTitles = document.querySelectorAll('.main_tabs__tit');
-          
-            tabTitles.forEach((title) => {
-              title.addEventListener('click', () => {
-                // Проверяем, есть ли у текущего заголовка класс .isActive
-                const isActive = title.classList.contains('isActive');
-          
-                // Убираем класс .isActive у всех заголовков
-                tabTitles.forEach((t) => t.classList.remove('isActive'));
-          
-                // Скрываем все содержимое вкладок
-                const tabContents = document.querySelectorAll('.main_tabs__content');
-                tabContents.forEach((content) => (content.style.display = 'none'));
-          
-                // Если у текущего заголовка не было класса .isActive, добавляем его
-                if (!isActive) {
-                  title.classList.add('isActive');
-                  
-                  // Отображаем соответствующее содержимое текущей вкладки
-                  const content = title.nextElementSibling;
-                  content.style.display = 'block';
-                }
-              });
+  constructor(){}
+  openTabs() {
+      document.addEventListener('DOMContentLoaded', function () {
+          const tabTitles = document.querySelectorAll('.main_tabs__tit');
+        
+          tabTitles.forEach((title, index) => {
+            title.addEventListener('click', () => {
+              // Убираем класс .isActive у всех заголовков
+              tabTitles.forEach((t) => t.classList.remove('isActive'));
+        
+              // Скрываем все содержимое вкладок
+              const tabContents = document.querySelectorAll('.main_tabs__content');
+              tabContents.forEach((content) => (content.style.display = 'none'));
+        
+              // Добавляем класс .isActive к текущему заголовку
+              title.classList.add('isActive');
+                
+              // Отображаем соответствующее содержимое текущей вкладки
+              const content = title.nextElementSibling;
+              content.style.display = 'block';
             });
-        });
-    }
-    init() {
-        this.openTabs();
-    }
+            
+            // По умолчанию активируем первый заголовок и его содержимое
+            if (index === 0) {
+              title.classList.add('isActive');
+              const content = title.nextElementSibling;
+              content.style.display = 'block';
+            }
+          });
+      });
+  }
+  init() {
+      this.openTabs();
+  }
 }
 
 export default MainTabs;
